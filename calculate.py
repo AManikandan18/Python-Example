077ff15f905e3c238b6d3fc6809384881973dc42
AManikandan18 <143077243+AManikandan18@users.noreply.github.com>
2024-07-17 23:44:26 +0530

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,12 @@
+a=int(input("enter which table you want: "))
+for i in range(1,11):
+    table=a*i
+    print(table)
+#ex:2;
+a=int(input("enter which table you want: "))
+for i in range(1,11):
+    table=a*i
+    print(i,"*",a,"=",table)
+    
+#ascii value calculate;
+print(ord("@"))
