b0bbe5d9346e2d1db4185b692c04929b4921d813
AManikandan18 <143077243+AManikandan18@users.noreply.github.com>
2024-07-25 23:45:23 +0530

    Add files via upload

---
+++ identity oprator.py
@@ -0,0 +1,12 @@
+a=int(input("Enter your first object: "))
+b=int(input("Enter your second object: "))
+c=int(input("Enter your third object: "))
+if(a is b):
+    print("value is true")
+else:
+    print("value is false")
+if(a is c):
+    print("value a and c is true")
+    #if(a is not c):
+        #print("value a and c is true ")
+    
