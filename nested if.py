a5595c526637922213ee99bdf0c47eed2bd7e3ca
AManikandan18 <143077243+AManikandan18@users.noreply.github.com>
2024-08-01 00:03:31 +0530

    Add files via upload

---
+++ nested if.py
@@ -0,0 +1,17 @@
+b=int(input("your budget: "))
+a=15000
+t=13000
+r=20000
+if(b>=r):
+    print("i want r15")
+elif(b>10000):
+    if(a<t):
+        print("i want access")
+    else:
+        print("i want tvs")
+elif(a==t):
+    print("i want access")
+else:
+    print("no bike")
+
+    
